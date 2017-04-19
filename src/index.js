532ee65a11751e41ed515a8ca886fe92f6ae0ad1
Lalit Thapa <mlalitthapa@gmail.com>
2017-04-19 23:10:12 +0545

    create an input field for search component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyBicImyrAtIqvUBS0rZZYZ1Je4aP_Aq9tw';
 
 // Create some html component
 const App = () => {
-    return <div>Hello World!</div>;
-}
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+};
 
 // Render the HTML component
 ReactDOM.render(<App />, document.querySelector('.container'));
