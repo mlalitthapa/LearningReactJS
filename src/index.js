2bbf65148a5f9e3cd0e601b7347d07347d224af9
Lalit Thapa <mlalitthapa@gmail.com>
2017-04-20 21:08:13 +0545

    change App component into class and send request to youtube on startup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBicImyrAtIqvUBS0rZZYZ1Je4aP_Aq9tw';
 
 // Create some html component
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
-};
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'Ozzy Man'}, (videos) => {
+            console.log(videos);
+            // this will be resolved as this.setState({ videos: videos }); (ES6 syntax)
+            this.setState({ videos });
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+}
 
 // Render the HTML component
 ReactDOM.render(<App />, document.querySelector('.container'));
