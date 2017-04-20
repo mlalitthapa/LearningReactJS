1e88fcf688a056a1fdb404a4c8792ae76dd0cfac
Lalit Thapa <mlalitthapa@gmail.com>
2017-04-20 23:15:52 +0545

    move app component to different file

---
+++ src/components/app.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './search_bar';
+import VideoDetail from './video_detail';
+import VideoList from './video_list';
+
+const API_KEY = 'AIzaSyBicImyrAtIqvUBS0rZZYZ1Je4aP_Aq9tw';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.searchVideo('Ozzy Man');
+    }
+
+    searchVideo(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar onSearchTermChanged={term => this.searchVideo(term)} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos}/>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar';
-import VideoDetail from './components/video_detail';
-import VideoList from './components/video_list';
+import App from './components/app';
 
-const API_KEY = 'AIzaSyBicImyrAtIqvUBS0rZZYZ1Je4aP_Aq9tw';
-
-// Create some html component
-class App extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        };
-
-        this.searchVideo('Ozzy Man');
-    }
-
-    searchVideo(term) {
-        YTSearch({key: API_KEY, term: term}, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <SearchBar onSearchTermChanged={term => this.searchVideo(term)} />
-                <VideoDetail video={this.state.selectedVideo}/>
-                <VideoList
-                    onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
-                    videos={this.state.videos}/>
-            </div>
-        );
-    }
-}
-
-// Render the HTML component
 ReactDOM.render(<App />, document.querySelector('.container'));
